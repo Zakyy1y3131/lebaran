a8aa4c80bd8e2f973d695142d5fee6e989e4bdbd
InsanoerZaki Faiq Istighfar <insanoerzaki@gmail.com>
2025-03-29 23:40:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function f1() {
     Swal.fire({
         title: "Selamat Hari Raya!",
         text: "Berbuat khilaf adalah sifat, meminta maaf adalah kewajiban, dan kembalinya fitrah adalah tujuan.\n\nMarilah kita melunturkan ego dan saling membuka pintu maaf.\n\nSelamat Hari Raya Lebaran! Semoga Allah SWT kembali mempertemukan kita di bulan Ramadan berikutnya.",
-       
         confirmButtonText: "Mohon Maaf Lahir Batin 🙏",
         confirmButtonColor: "#008cff"
     }).then((result) => {
@@ -16,14 +15,18 @@ function f1() {
 
 function f() {
     let btn = document.getElementById("noBtn");
+    btn.style.position = "relative";
     if (flag == 1) {
-        btn.style.transform = "translate(100px, 50px)";
+        btn.style.left = "50px";
+        btn.style.top = "20px";
         flag = 2;
     } else if (flag == 2) {
-        btn.style.transform = "translate(-100px, -50px)";
+        btn.style.left = "-50px";
+        btn.style.top = "-20px";
         flag = 3;
     } else if (flag == 3) {
-        btn.style.transform = "translate(0, 0)";
+        btn.style.left = "0px";
+        btn.style.top = "0px";
         flag = 1;
     }
 }
